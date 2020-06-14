bfa0e19c3d8c1c2651e295cc86d30dd452b7eee9
Maksim Laptenko <laptenko0811@icloud.com>
2020-06-14 21:54:09 +0300

    correction

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,10 @@
 public class CreditPaymentService {
-    public int calculate(int summa , int month, double percent){
+    public int calculate(int summa, int month, double percent) {
         double i = percent / 100 / 12;
-        double z = Math.pow(i+1,month);
-        double P = summa * (i + ( i / ( z - 1 )));
+        double z = Math.pow(i + 1, month);
+        double P = summa * (i + (i / (z - 1)));
         int w = (int) P;
         return w;
     }
-
-
 }
 
